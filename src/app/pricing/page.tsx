e8d63a389488bd2770da67b891c7ab0a840748fe
Ekene <megafat142@gmail.com>
2024-01-08 19:57:34 +0100

    chore: changed currency format

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,24 +1,35 @@
 import { PriceCards } from '@/components/component/PriceCards'
 import { env } from '@/env'
+import axios from 'axios'
 import React from 'react'
 
 export default async function PricingPage() {
   const subscription = await fetch('https://api.paystack.co/subscription', {
     method:"GET",
     headers:{
-      authorization:`Bearer ${env.PAYSTACK_SECRET_KEY}`
+      Authorization:`Bearer ${env.PAYSTACK_SECRET_KEY}`
     }
   })
 
   const awaitedSub = await subscription.json() as {status:boolean; data:[{
     status:string
   }]}
+
+  const plan:Awaited<{data:{data:{
+    amount:number
+  }}}> = await axios('https://api.paystack.co/plan/PLN_t0gluvovnmtmuam', {
+    headers:{
+      Authorization:`Bearer ${env.PAYSTACK_SECRET_KEY}`
+    }
+  }) 
+
+  
   
   console.log('subs',awaitedSub.data[0].status);
   
   return (
     <div>
-        <PriceCards />
+        <PriceCards amount={plan.data.data.amount} />
     </div>
   )
 }
